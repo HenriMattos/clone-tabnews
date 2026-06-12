eee8fe9efb88afaf4d6c5da5f88a4ba3a9273dc6
Henrique Mattos <145288353+HenriMattos@users.noreply.github.com>
2026-06-12 01:57:49 +0000

    adiciona a pasta pages

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Olá Gustavo, essa mensagem eu fiz dia 11/06, ao iniciar os estudos pelo curso.dev, espero que esteja mais avançado nos conhecimentos.</h1>
+}
+
+export default Home;
